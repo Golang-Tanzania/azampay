1a7a232378fbe888e0b9981506a49a13d5659c13
AvicennaJr <justfuad@protonmail.com>
2023-02-20 19:59:53 +0300

    Add callback and payment partner examples

--- example/main.go
+++ example/main.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+
+	// initialize
 	var transactionTester GoAzam.APICONTEXT
 
 	transactionTester.LoadKeys("config.json")
 	transactionTester.GenerateSessionID()
 
+	// test mobile checkout
 	testMobileCheckout := make(map[string]string)
 
 	testMobileCheckout["accountNumber"] = "0700000000"
@@ -21,6 +24,7 @@ func main() {
 
 	fmt.Println(transactionTester.MobileCheckout(testMobileCheckout))
 
+	// test bank checkout
 	testBankCheckout := make(map[string]string)
 
 	testBankCheckout["amount"] = "10000"
@@ -32,4 +36,25 @@ func main() {
 	testBankCheckout["ReferenceID"] = "123"
 
 	fmt.Println(transactionTester.BankCheckout(testBankCheckout))
+
+	// test Callback
+
+	testCallback := make(map[string]string)
+
+	testCallback["msisdn"] = "0178334"
+	testCallback["amount"] = "2000"
+	testCallback["message"] = "testing callback"
+	testCallback["utilityref"] = "1282-123"
+	testCallback["operator"] = "Airtel"
+	testCallback["reference"] = "123-123"
+	testCallback["transactionstatus"] = "success"
+	testCallback["submerchantAcc"] = "01723113"
+
+	testCallbackURL := "" // You need to set a webhook or fill provided URL
+	fmt.Println(transactionTester.Callback(testCallback, testCallbackURL))
+
+	// test Payment Partner
+
+	fmt.Println(transactionTester.PaymentPartners())
+
 }
