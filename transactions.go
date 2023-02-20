e0d2aa41e32435d54179960234a3a7e7ce0d6755
AvicennaJr <justfuad@protonmail.com>
2023-02-20 19:56:56 +0300

    PaymentPartners should use get and not post
    All requests will accept the base url as a parameter

--- transactions.go
+++ transactions.go
@@ -1,25 +1,25 @@
 package GoAzam
 
 func (api *APICONTEXT) MobileCheckout(data map[string]string) string {
-	return api.sendRequest("/azampay/mno/checkout", data)
+	return api.sendRequest(api.BaseURL, "/azampay/mno/checkout", data)
 }
 
 func (api *APICONTEXT) BankCheckout(data map[string]string) string {
-	return api.sendRequest("/azampay/bank/checkout", data)
+	return api.sendRequest(api.BaseURL, "/azampay/bank/checkout", data)
 }
 
-func (api *APICONTEXT) Callback(data map[string]string) string {
-	return api.sendRequest("/api/v1/Checkout/Callback", data)
+func (api *APICONTEXT) Callback(data map[string]string, url string) string {
+	return api.sendRequest(url, "/api/v1/Checkout/Callback", data)
 }
 
-func (api *APICONTEXT) PaymentPartners(data map[string]string) string {
-	return api.sendRequest("/api/v1/Partner/GetPaymentPartners", data)
+func (api *APICONTEXT) PaymentPartners() string {
+	return api.getRequest(api.BaseURL, "/api/v1/Partner/GetPaymentPartners")
 }
 
 /*
 // This needs its own handling
 
 func (api *APICONTEXT) PostCheckout(data map[string]interface{}) string {
-	return api.sendRequest("/api/v1/Partner/PostCheckout", data)
+	return api.sendRequest(api.BaseURL, "/api/v1/Partner/PostCheckout", data)
 }
 */
