5a6dee15d36e4e6c2b92c271cef7d120e355e322
AvicennaJr <justfuad@protonmail.com>
2023-02-22 16:44:52 +0300

    separate error logic

---
+++ errors.go
@@ -0,0 +1,51 @@
+package GoAzam
+
+import "fmt"
+
+type (
+	// List of errors eg: missing a field, wrong type etc
+	ErrorList map[string]any
+
+	// Bad request error returned when there's a validation error
+	BadRequestError struct {
+		// Status code 400
+		Status int64 `json:"status"`
+
+		// List of errors
+		Errors map[string]any `json:"errors"`
+
+		// One or more validation errors occurred
+		Title string `json:"title"`
+
+		// See https://tools.ietf.org/html/rfc7231#section-6.5.1
+		Type string `json:"type"`
+
+		// Error ID
+		TraceID string `json:"traceId"`
+	}
+
+	// Status code 417: Please provide authorization
+	Unauthorized struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+	}
+)
+
+func (bd *BadRequestError) Error() string {
+	if bd != nil {
+		var errorList string
+		for k, v := range bd.Errors {
+			errorList = errorList + fmt.Sprintf("\t\t\t%s:\t%v\n", k, v)
+		}
+		return fmt.Sprintf("Bad Request Error: %d\t%s\n%s", bd.Status, bd.Title, errorList)
+	}
+	return ""
+}
+
+func (ua *Unauthorized) Error() string {
+	if ua != nil {
+		return fmt.Sprintf("Unauthorized: %s", ua.Message)
+	}
+
+	return ""
+}
