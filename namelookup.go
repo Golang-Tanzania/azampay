8602133d7d9877d8ed0952174cb2e79c0e487490
AvicennaJr <justfuad@protonmail.com>
2023-02-26 01:55:55 +0300

    Add schema for name lookup

---
+++ namelookup.go
@@ -0,0 +1,10 @@
+package GoAzam
+
+// The endpoint to lookup the name associated with
+// a bank account or mobile money
+type NameLookup struct {
+	// Bank account number or mobile money number
+	BankName string `json:"bankName"`
+	// Bank name or mobile money name associated with the account
+	AccountNumber string `json:"accountNumber"`
+}
